eaf400dd3e4c9f58119af4cf3db5f62c459dccbd
Luky Drum <lukbubi12@gmail.com>
2023-12-01 19:34:59 +0100

    Add solution to Day 1 Part 2

--- aoc2023/src/days/day1.rs
+++ aoc2023/src/days/day1.rs
@@ -16,10 +16,7 @@ impl Day for Day1 {
             .iter()
             .filter(|line| !line.is_empty())
             .map(|line| {
-                
-                let nums: Vec<char> = line.chars()
-                    .filter(|c| c.is_digit(10))
-                    .collect();
+                let nums: Vec<char> = line.chars().filter(|c| c.is_digit(10)).collect();
 
                 let mut num = String::from("");
                 num.push(*nums.first().unwrap());
@@ -31,6 +28,35 @@ impl Day for Day1 {
     }
 
     fn part2(&self) -> i32 {
-        -1
+        self.lines
+            .iter()
+            .filter(|line| !line.is_empty())
+            .map(|line| {
+                let replacements = vec![
+                    ("one", "o1e"),
+                    ("two", "t2o"),
+                    ("three", "t3e"),
+                    ("four", "f4r"),
+                    ("five", "f5e"),
+                    ("six", "s6x"),
+                    ("seven", "s7n"),
+                    ("eight", "e8t"),
+                    ("nine", "n9n")
+                ];
+                
+                let mut new_line = line.clone();
+                for rep in replacements {
+                    new_line = new_line.replace(rep.0, rep.1);
+                }
+
+                let nums: Vec<char> = new_line.chars().filter(|c| c.is_digit(10)).collect();
+
+                let mut num = String::from("");
+                num.push(*nums.first().unwrap());
+                num.push(*nums.last().unwrap());
+
+                num.parse::<i32>().unwrap()
+            })
+            .fold(0, |sum, num| sum + num)
     }
 }
