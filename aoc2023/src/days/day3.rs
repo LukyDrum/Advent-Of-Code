45de292475f7caf77bb27e95590b350c029051e9
Lukas Bubinek <lukbubi12@gmail.com>
2023-12-04 15:30:33 +0100

    Day 3 part 1 solution

---
+++ aoc2023/src/days/day3.rs
@@ -0,0 +1,154 @@
+use std::vec;
+
+use crate::day::Day;
+
+struct Number {
+    value: i32,
+    row: i32,
+    start_col: i32,
+    end_col: i32,
+}
+
+struct Gear {
+    row: i32,
+    col: i32,
+}
+
+pub struct Day3 {
+    pub lines: Vec<String>,
+}
+
+impl Day for Day3 {
+    fn new() -> Self {
+        Day3 {
+            lines: Self::read_input(3),
+        }
+    }
+
+    fn part1(&self) -> i32 {
+        let mut char_matrix: Vec<Vec<u8>> = Vec::new();
+
+        let example_lines = vec![
+            "467..114..",
+            "...*......",
+            "..35..633.",
+            "......#...",
+            "617*......",
+            ".....+.58.",
+            "..592.....",
+            "......755.",
+            "...$.*....",
+            ".664.598..",
+            "..........",
+            "......$42.",
+            "..........",
+        ];
+
+        for line in &self.lines {
+            let mut bytes = line.clone().into_bytes();
+            bytes.push(b'\n');
+
+            char_matrix.push(bytes);
+        }
+
+        let numbers = self.get_numbers(&char_matrix);
+
+        let mut ans = 0;
+        for num in &numbers {
+            // println!("{} @ ({}, {} -> {})", num.value, num.row, num.start_col, num.end_col);
+            for col in num.start_col..(num.end_col + 1) {
+                if self.is_position_adjecent_to_symbol(&char_matrix, (num.row, col)) {
+                    ans += num.value;
+                    break;
+                }
+            }
+        }
+
+        ans
+    }
+
+    fn part2(&self) -> i32 {
+        -1
+    }
+}
+
+impl Day3 {
+
+    fn is_position_adjecent_to_symbol(
+        &self,
+        char_matrix: &Vec<Vec<u8>>,
+        position: (i32, i32),
+    ) -> bool {
+        let offsets = vec![-1, 0, 1];
+
+        for o_row in &offsets {
+            for o_col in &offsets {
+                let row = position.0 as i32 + o_row;
+                let col = position.1 as i32 + o_col;
+
+                if row < 0 || col < 0 {
+                    continue;
+                }
+
+                let offseted = char_matrix.get(row as usize);
+                if offseted.is_none() {
+                    continue;
+                }
+                let offseted = offseted.unwrap().get(col as usize);
+                if offseted.is_none() {
+                    continue;
+                }
+
+                let c = *offseted.unwrap();
+
+                if !c.is_ascii_digit() && c != b'.' && c != b'\n' {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+
+    fn get_numbers(&self, char_matrix: &Vec<Vec<u8>>) -> Vec<Number> {
+        let mut numbers: Vec<Number> = Vec::new();
+
+        let mut num: String = "".to_string();
+
+        let mut row_index: i32 = 0;
+        let mut col_index: i32 = 0;
+
+        let mut start_col = 0;
+
+        for row in char_matrix {
+            col_index = 0;
+            for c in row {
+                let c = *c as char;
+                if c.is_ascii_digit() && c != '\n' {
+                    if num.is_empty() {
+                        start_col = col_index;
+                    }
+
+                    num.push(c);
+                } else if !num.is_empty() {
+                    let number = Number {
+                        value: num.parse::<i32>().unwrap(),
+                        row: row_index,
+                        start_col: start_col,
+                        end_col: col_index - 1,
+                    };
+                    numbers.push(number);
+                    num = "".to_string();
+                } else {
+                    num = "".to_string();
+                }
+
+                col_index += 1;
+            }
+            num = "".to_string();
+            row_index += 1;
+        }
+
+        numbers
+    }
+}
