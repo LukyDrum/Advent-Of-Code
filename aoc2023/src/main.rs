95d314550efeccce8b781fc7c2ab3ce13dff8490
Luky Drum <lukbubi12@gmail.com>
2023-12-10 18:34:27 +0100

    Edit printout table

--- aoc2023/src/main.rs
+++ aoc2023/src/main.rs
@@ -12,15 +12,16 @@ fn main() {
     let all_days: Vec<Box<dyn Day>> = vec![Day1::new_box(), Day2::new_box(), Day3::new_box(), Day4::new_box()];
 
     // This will print the output in a table
-    println!("Day\t Part 1 \t Part 2");
-    println!("__________________________________________");
+    println!("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+    println!("┃ Day \t┃ Part 1 \t┃ Part 2 \t ┃");
+    println!("┡━━━━━━━┯━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━━━┩");
     let mut i = 1;
     for d in all_days {
-        print!("Day {}\t| ", i);
-        println!("\t{}\t|\t{}", d.part1(), d.part2());
+        print!("│ {}\t│ ", i);
+        println!("\t{}\t│\t{}\t │", d.part1(), d.part2());
 
         i += 1;
     }
 
-    println!("__________________________________________\n");
+    println!("└───────┴───────────────┴────────────────┘\n")
 }
