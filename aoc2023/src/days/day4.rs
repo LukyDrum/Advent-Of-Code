c06f68796e8229199b9efd4a014612f7c9c19440
Luky Drum <lukbubi12@gmail.com>
2023-12-10 16:25:10 +0100

    Setup for day 4

---
+++ aoc2023/src/days/day4.rs
@@ -0,0 +1,22 @@
+use crate::day::Day;
+
+
+pub struct Day4 {
+    pub lines: Vec<String>,
+}
+
+impl Day for Day4 {
+    fn new() -> Self {
+        Day4 {
+            lines: Self::read_input(4),
+        }
+    }
+
+    fn part1(&self) -> i32 {
+        -1
+    }
+
+    fn part2(&self) -> i32 {
+        -1
+    }
+}
