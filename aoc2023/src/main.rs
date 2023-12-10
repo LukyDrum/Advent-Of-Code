3984a93d74a53de2583711c1f36c5993f3b73b2e
Luky Drum <lukbubi12@gmail.com>
2023-12-10 16:25:37 +0100

    Setup for day 4 fix

--- aoc2023/src/main.rs
+++ aoc2023/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     println!("\nWelcome to Advent Of Code 2023!\n");
 
     // Vector of all days
-    let all_days: Vec<Box<dyn Day>> = vec![Day1::new_box(), Day2::new_box(), Day3::new_box()];
+    let all_days: Vec<Box<dyn Day>> = vec![Day1::new_box(), Day2::new_box(), Day3::new_box(), Day4::new_box()];
 
     // This will print the output in a table
     println!("Day\t Part 1 \t Part 2");
