3ac8a172be63f46150dfd09eb0d11a31eef80ca0
Lukas Bubinek <lukbubi12@gmail.com>
2023-12-12 18:53:25 +0100

    Make setup method of day 6

--- aoc2023/src/days/day6.rs
+++ aoc2023/src/days/day6.rs
@@ -1,14 +1,17 @@
 use crate::day::Day;
 
-
 pub struct Day6 {
     pub lines: Vec<String>,
+    times: Vec<i32>,
+    distances: Vec<i32>,
 }
 
 impl Day for Day6 {
     fn new() -> Self {
         Day6 {
             lines: Self::read_input(6, true),
+            times: Vec::new(),
+            distances: Vec::new(),
         }
     }
 
@@ -18,5 +21,24 @@ impl Day for Day6 {
 
     fn part2(&self) -> i32 {
         -1
+    }
+
+    fn setup(&mut self) -> () {
+        let ls: Vec<Vec<i32>> = self
+            .lines
+            .iter()
+            .map(|line| {
+                line.split_ascii_whitespace().collect::<Vec<&str>>()[1..]
+                    .iter()
+                    .map(|num| num.parse::<i32>().unwrap()).collect::<Vec<i32>>()
+            })
+            .collect();
+
+        for n in &ls[0] {
+            self.times.push(*n);
+        }
+        for n in &ls[1] {
+            self.distances.push(*n);
+        }
     }
 }
